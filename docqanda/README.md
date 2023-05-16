1d8b32eee4d0c70ac85416ce202f527961e3546a
Chris Padwick <chris.padwick@bluerivert.com>
2023-05-15 20:21:27 -0700

    added openai support

--- docqanda/README.md
+++ docqanda/README.md
@@ -4,6 +4,8 @@ A demo showing how to run document Q&A on a text file with a user hosted Large L
 
 In this demo a text file called `shakespeare.txt` which contains all of Shakespeare's plays is indexed using HuggingFace embeddings into a Vectorstore.  Then one of the pre-downloaded models is loaded, and a RetreivalQA chain from langchain is used to run the provided query on the document.  The response from the model is printed out on the screen.
 
+OpenAI models can be used with this demo by selecting `--model openai` and answering `Y` to the warning.
+
 ## Installation
 
 Clone the repo and create a virtual env as follows:
@@ -29,10 +31,20 @@ This demo supports a few different models.  Download one or more of them and sti
 - [ggml-alpaca-7b-q4.bin](https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/tree/main)
 - [gpt4all-lora-quantized.bin](https://huggingface.co/aryan1107/gpt4all-llora/resolve/main/gpt4all-lora-quantized.bin)
 
+## Optional OpenAI Api Key
+
+If you choose `--model openai` then you will need an OpenAI API key for this demo to work.  To get a key:
+
+* Create an account on platform.openai.com
+* Under "Personal" click "View API Keys"
+* Create a new secret key, and copy it to your clipboard.
+* You will need to save this secret key somewhere.
+
 ## Running The Code
 
 - Source the virtual env `source venv/bin/activate`
 - Run the demo with `python docqanda.py --inputfile shakespeare.txt`
   - Run new queries using the --query flag
   - Adjust --threads for number of threads and --n_ctx for the context length
+  - Set the OpenAI enviroment variable (bash) with `export OPENAI_API_KEY="your key goes here"` if you choose --model openai
 
--- docqanda/docqanda.py
+++ docqanda/docqanda.py
@@ -3,24 +3,35 @@ import multiprocessing
 import os
 from typing import Union
 
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
 from langchain.vectorstores import FAISS, Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.base import VectorStoreRetriever
 from langchain.document_loaders import TextLoader
 from langchain.chains import RetrievalQA
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import GPT4All, LlamaCpp, OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.stdout import StdOutCallbackHandler
 
 
-def create_embeddings(textfile: str) -> VectorStoreRetriever:
+def warn_openai(model: str):
+    if model == "openai":
+        msg = """
+        WARNING: Using the OpenAI model will cost money, and will share your data with
+        OpenAI.  Review OpenAIs terms of service at https://openai.com/policies/terms-of-use
+        before proceeding.  Type Y to continue.
+        """
+        ans = input(msg)
+        if ans != "Y":
+            exit(1)
+
+def create_embeddings(textfile: str, isOpenAI: bool) -> VectorStoreRetriever:
     """
     Creates embeddings from the given text file and returns a VectorStoreRetriever object to perform
     vector searches on the embeddings.
     :param textfile: The path to the text file containing the documents to be embedded.
-    :type textfile: str
+    :param isOpenAI: bool controlling whether or not to use OpenAI's embeddings
     :return: A VectorStoreRetriever object containing the embeddings of the documents in the text file.
     :rtype: VectorStoreRetriever
     """
@@ -32,11 +43,14 @@ def create_embeddings(textfile: str) -> VectorStoreRetriever:
     text_split = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=0)
     text = text_split.split_documents(loader)
 
-    hf_embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    if isOpenAI:
+        embeddings = OpenAIEmbeddings()
+    else:
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
 
-    docsearch = Chroma.from_documents(text, hf_embeddings)
+    docsearch = Chroma.from_documents(text, embeddings)
     return docsearch.as_retriever()
 
 
@@ -50,9 +64,11 @@ def load_model(model_name: str, n_ctx: int, n_threads: int) -> Union[GPT4All, Ll
     """
     callbacks = [StreamingStdOutCallbackHandler(), StdOutCallbackHandler()]
     n_threads = multiprocessing.cpu_count() if n_threads == -1 else n_threads
-    modelfile = f"models/{model_name}"
-    if not os.path.exists(modelfile):
-        raise ValueError(f"Invalid model path: {modelfile}")
+
+    if not model_name == "openai":
+        modelfile = f"models/{model_name}"
+        if not os.path.exists(modelfile):
+            raise ValueError(f"Invalid model path: {modelfile}")
 
     if model_name in ("gpt4all-lora-quantized.bin", "ggml-gpt4all-l13b-snoozy.bin"):
         model = GPT4All(
@@ -70,6 +86,8 @@ def load_model(model_name: str, n_ctx: int, n_threads: int) -> Union[GPT4All, Ll
         model = LlamaCpp(
             model_path=modelfile, callbacks=callbacks, n_ctx=n_ctx, n_threads=n_threads
         )
+    elif model_name == "openai":
+        model = OpenAI()
     else:
         raise ValueError(f"Invalid model name: {model_name}")
 
@@ -93,6 +111,7 @@ if __name__ == "__main__":
             "ggml-alpaca-7b-q4.bin",
             "ggml-gpt4all-l13b-snoozy.bin",
             "gpt4all-lora-quantized.bin",
+            "openai"
         ],
         default="ggml-gpt4all-j-v1.3-groovy.bin",
         help="LLM Model name to use.  Must be downloaded to the models directory before starting.",
@@ -106,8 +125,9 @@ if __name__ == "__main__":
         help="Query",
     )
     args = parser.parse_args()
-    
-    docsearch = create_embeddings(args.inputfile)
+
+    warn_openai(args.model)
+    docsearch = create_embeddings(args.inputfile, args.model == "openai")
     model = load_model(args.model, args.n_ctx, args.threads)
 
     qa = RetrievalQA.from_chain_type(llm=model, chain_type="stuff", retriever=docsearch)
