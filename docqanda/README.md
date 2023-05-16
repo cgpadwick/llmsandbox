498673c367bbd8dafcdfaa3df7dcdbb3ec4a1066
Chris Padwick <chris.padwick@bluerivert.com>
2023-05-15 19:22:44 -0700

    refactor docqanda.py, add README

---
+++ docqanda/README.md
@@ -0,0 +1,38 @@
+# Document Q&A On A Given Text File
+
+A demo showing how to run document Q&A on a text file with a user hosted Large Language Model.  This can be useful for companies or individuals who are not comfortable sending their private data to cloud hosted services (e.g. OpenAI).
+
+In this demo a text file called `shakespeare.txt` which contains all of Shakespeare's plays is indexed using HuggingFace embeddings into a Vectorstore.  Then one of the pre-downloaded models is loaded, and a RetreivalQA chain from langchain is used to run the provided query on the document.  The response from the model is printed out on the screen.
+
+## Installation
+
+Clone the repo and create a virtual env as follows:
+
+`virtualenv -p python3.8 venv`
+
+You might need to install python3.8 and virtualenv too (exercise left to the reader).
+
+Source the virtual env:
+
+`source venv/bin/activate`
+
+Install the pip packages:
+
+`pip install -r requirements.txt`
+
+## Downloading The Models
+
+This demo supports a few different models.  Download one or more of them and stick them in the `models` folder.
+
+* [ggml-gpt4all-j-v1.3-groovy.bin](https://gpt4all.io/models/ggml-gpt4all-j-v1.3-groovy.bin)
+* [ggml-gpt4all-l13b-snoozy.bin] (https://gpt4all.io/models/ggml-gpt4all-l13b-snoozy.bin)
+* [ggml-alpaca-7b-q4.bin] (https://huggingface.co/Sosaka/Alpaca-native-4bit-ggml/tree/main)
+* [gpt4all-lora-quantized.bin](https://huggingface.co/aryan1107/gpt4all-llora/resolve/main/gpt4all-lora-quantized.bin)
+
+## Running The Code
+
+* Source the virtual env `source venv/bin/activate`
+* Run the demo with `python docqanda.py --inputfile shakespeare.txt`
+** Run new queries using the --query flag
+** Adjust --threads for number of threads and --n_ctx for the context length
+
--- docqanda/docqanda.py
+++ docqanda/docqanda.py
@@ -1,65 +1,114 @@
+import argparse
+import multiprocessing
+import os
+from typing import Union
+
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS, Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.base import VectorStoreRetriever
 from langchain.document_loaders import TextLoader
 from langchain.chains import RetrievalQA
-from langchain.llms import GPT4All
-from langchain.llms import LlamaCpp
+from langchain.llms import GPT4All, LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.callbacks.stdout import StdOutCallbackHandler
 
-textfile = "input.txt"
-loader = TextLoader(textfile).load()
-print(loader)
-text_split = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=0)
-text = text_split.split_documents(loader)
 
-hf_embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+def create_embeddings(textfile: str) -> VectorStoreRetriever:
+    """
+    Creates embeddings from the given text file and returns a VectorStoreRetriever object to perform
+    vector searches on the embeddings.
+    :param textfile: The path to the text file containing the documents to be embedded.
+    :type textfile: str
+    :return: A VectorStoreRetriever object containing the embeddings of the documents in the text file.
+    :rtype: VectorStoreRetriever
+    """
 
-# vectorstore = FAISS.from_documents(text, hf_embeddings)
-# print(vectorstore)
+    if not os.path.exists(path=textfile):
+        raise FileNotFoundError(f"The file {textfile} does not exist.")
 
-docsearch = Chroma.from_documents(text, hf_embeddings)
+    loader = TextLoader(textfile).load()
+    text_split = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=0)
+    text = text_split.split_documents(loader)
 
-# modelfile = '/home/chrispadwick/code/gpt4all-ui/models/gpt4all-lora-quantized.bin'
-# modelfile = 'gptjmodels/ggml-gpt4all-j-v1.3-groovy.bin'
-# modelfile = 'gptjmodels/ggml-gpt4all-j.bin'
-modelfile = "models/ggml-alpaca-7b-q4.bin"
+    hf_embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
-callbacks = [StreamingStdOutCallbackHandler(), StdOutCallbackHandler()]
-# model = GPT4All(model=modelfile, callbacks=callbacks, n_ctx=500, n_threads=8)
-# model = GPT4All(model=modelfile, backend='gptj', callbacks=callbacks, n_ctx=500, n_threads=8)
-model = LlamaCpp(model_path=modelfile, callbacks=callbacks, n_ctx=1024, n_threads=12)
+    docsearch = Chroma.from_documents(text, hf_embeddings)
+    return docsearch.as_retriever()
 
-# This doesn't work for some reason.
-qa = RetrievalQA.from_chain_type(
-    llm=model, chain_type="refine", retriever=docsearch.as_retriever()
-)
-query = "Does Lady MacBeth appear in the text?"
-print(qa.run(query))
 
-prompt_template = """
-{context}
+def load_model(model_name: str, n_ctx: int, n_threads: int) -> Union[GPT4All, LlamaCpp]:
+    """
+    Loads and returns a machine learning model based on the provided `model_name`.
 
-Question: {question}
+    :param model_name: str, the name of the model to load.
+    :return: Union[GPT4All, LlamaCpp], the loaded model object.
+    :raises ValueError: if an invalid `model_name` is provided.
+    """
+    callbacks = [StreamingStdOutCallbackHandler(), StdOutCallbackHandler()]
+    n_threads = multiprocessing.cpu_count() if n_threads == -1 else n_threads
+    modelfile = f"models/{model_name}"
+    if not os.path.exists(modelfile):
+        raise ValueError(f"Invalid model path: {modelfile}")
 
-Explain your answer in detail.
-"""
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
+    if model_name in ("gpt4all-lora-quantized.bin", "ggml-gpt4all-l13b-snoozy.bin"):
+        model = GPT4All(
+            model=modelfile, callbacks=callbacks, n_ctx=n_ctx, n_threads=n_threads
+        )
+    elif model_name == "ggml-gpt4all-j-v1.3-groovy.bin":
+        model = GPT4All(
+            model=modelfile,
+            backend="gptj",
+            callbacks=callbacks,
+            n_ctx=n_ctx,
+            n_threads=n_threads,
+        )
+    elif model_name == "ggml-alpaca-7b-q4.bin":
+        model = LlamaCpp(
+            model_path=modelfile, callbacks=callbacks, n_ctx=n_ctx, n_threads=n_threads
+        )
+    else:
+        raise ValueError(f"Invalid model name: {model_name}")
 
-chain_type_kwargs = {"prompt": PROMPT}
-qa = RetrievalQA.from_chain_type(
-    llm=model,
-    chain_type="stuff",
-    retriever=docsearch.as_retriever(),
-    chain_type_kwargs=chain_type_kwargs,
-)
+    return model
 
-# query = "Does Lady Macbeth appear in the text?"
-# print(qa.run(query))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inputfile",
+        required=True,
+        type=str,
+        help="Input text file to index",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        required=False,
+        choices=[
+            "ggml-gpt4all-j-v1.3-groovy.bin",
+            "ggml-alpaca-7b-q4.bin",
+            "ggml-gpt4all-l13b-snoozy.bin",
+            "gpt4all-lora-quantized.bin",
+        ],
+        default="ggml-gpt4all-j-v1.3-groovy.bin",
+        help="LLM Model name to use.  Must be downloaded to the models directory before starting.",
+    )
+    parser.add_argument("--n_ctx", type=int, default=1024, help="Context size")
+    parser.add_argument("--threads", type=int, default=-1, help="Number of threads")
+    parser.add_argument(
+        "--query",
+        type=str,
+        default="Does MENENIUS mention bats and clubs? Explain your answer",
+        help="Query",
+    )
+    args = parser.parse_args()
+    
+    docsearch = create_embeddings(args.inputfile)
+    model = load_model(args.model, args.n_ctx, args.threads)
+
+    qa = RetrievalQA.from_chain_type(llm=model, chain_type="stuff", retriever=docsearch)
+    print(qa.run(args.query))
